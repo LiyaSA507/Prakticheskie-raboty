31d794bd6e6427fe2b12f003ebc86c72cca7fb33
LiyaSA507 <59200547+LiyaSA507@users.noreply.github.com>
2019-12-26 18:38:14 +0700

    Add files via upload

---
+++ Практическая 3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double symm = 0, znamenatel = 1;
+            while(symm < 0.7)
+            {
+                symm += 1 / (znamenatel * (znamenatel + 2));
+                znamenatel++;
+            }
+            Console.WriteLine(symm);
+        }
+    }
+}
